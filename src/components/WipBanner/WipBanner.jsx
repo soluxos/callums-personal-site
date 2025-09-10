7c0f73740516bf68851dead162e424843f903a70
Callum Harrod <callumharrod1994@hotmail.co.uk>
2025-09-10 13:36:51 +0100

    Changed the WIP banner

--- src/components/WipBanner/WipBanner.jsx
+++ src/components/WipBanner/WipBanner.jsx
@@ -5,10 +5,7 @@ export default function WipBanner() {
   return (
     <div className="bg-[#FF2B00] min-h-6 py-1 font-departure-mono uppercase flex flex-col justify-center items-center w-full px-10 md:px-15 lg:px-20">
       <p className="text-white text-[12px] font-departure-mono uppercase text-center">
-        Work in progress - Check progress on{" "}
-        <Link href="https://github.com/soluxos/callum-site-2025" className="underline">
-          GitHub
-        </Link>
+        Work in progress - Only started building this recently. Stay tuned for updates.
       </p>
     </div>
   );
